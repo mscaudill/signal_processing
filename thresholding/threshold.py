2b2dc95f92aea10d9d226390cd925f715b2b6b4d
Holly McGowan <mcgowah@stthom.edu>
2019-03-15 14:59:28 -0500

    Improved stat_estimator
    the statistic estimator now works with eeg data and with multiple statistics

--- thresholding/threshold.py
+++ thresholding/threshold.py
@@ -1,39 +1,24 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
-
-def rms(data,place=-1,axis=0):
+def rms(samples,axis=0):
     """Returns the root mean square statistic for samples.
     Args:
         samples (array_like):     array containing numbers whose rms is
-                                  desired
-        place (int):              if given matrix, and desired 
-                                  place allows you to select
-                                  an array from the matrix  
+                                  desired  
         axis (int):               axis along which rms is computed
                                   (Default=0)
     Returns: RMS (float)
     """
-    if place<0:
-        samples=data
-    else:
-        samples=data[place]
     result=np.mean(samples**2,axis=axis)
     return np.sqrt(result)
 
-def coast(data,place=-1):
+def coast(sample):
     """Returns the coast statistic for a sample.
     Args:
-        data (array_like):        array or matrix containing numbers whose 
-                                  coast is desired
-        place (int):              if given matrix, and desired 
-                                  place allows you to select
-                                  an array from the matrix
+        data (array_like):        array containing numbers whose coast is desired
     Returns: coast (float)
     """
-    if place<0:
-        sample=data
-    else:
-        sample=data[place]
     suma=0
     lgth=len(sample)
     for segment,old_segment in zip(sample[1:],sample):
@@ -42,8 +27,9 @@ def coast(data,place=-1):
 
 def ampcorr(data,place):
     """Returns the amplitude correlation statistic for three segments of data.
+    ***this will not work in the stat_estimator funtion***
     Args:
-        data (array_like):        array or matrix containing numbers whose 
+        data (array_like):        matrix containing numbers whose 
                                   coast is desired
         place (int):              place allows you to select
                                   an array from the matrix
@@ -65,7 +51,7 @@ def threshold(data, statistics, thresholds, stat_estimator, window_size=None,
                                             statistics are desired.
         statistics (func or seq of funcs):  callables returning statistic(s)
         thresholds (seq floats) 
-        initer (func):                      callable returning initial stats
+        stat_estimator (func):              callable returning initial stats
         window_size (int):                  window_size in samples over
                                             which statistic will be determined
         num_cores (int):                    number of processing cores used
@@ -77,46 +63,87 @@ def threshold(data, statistics, thresholds, stat_estimator, window_size=None,
     pass
 
 
-def stat_estimator(data,statistics,window_size,threshold_mean, threshold_win
+def generator(data,place,window_size):
+    return data[place*window_size:(place+1)*window_size]
+
+def stat_estimator(data,statistics,window_size,percent_tolerance, threshold_win
                    ):
     """Caluclates an estimated statistic by randomly sampling chunks 
-        until reaching a threshold range for the mean of that statistic
-        my next step will be to make this work with multiple statistics
+        until the difference the the mean for the current block of windows is
+        within the tolereance percentage for the first statistic
+        **works with eeg object if it only has one channel
     Args:
-        data(array):                    array
-        statistics(funtion):            statistics to calculate
-        window_size(int):                    the data will be split into chunks 
+        data(array_like):               array
+        statistics(funtion):            list of statistics to calculate
+        window_size(int):               the data will be split into chunks 
                                         this size
-        threshold_mean(int or float):   threshold value for the mean, 
-                                        stops the calculations
+        percent_tolerance(int or float):tolerance value for the mean using
+                                        percentage, stops the calculations
         threshold_win(int):             how large of a sample to look at when
                                         calculating the threshold
     
     Prints the difference and the number of samples used 
         *this is only temporary while I work on it.
-    Returns: returns estimated statistic (float)
+    Returns: returns estimated statistics in an array (float)
     """
     #reshapes the data into an array and cuts off the uneven segment at the end
-    data=data[0:len(data)-len(data)%window_size].reshape(
-            int(len(data)/window_size),window_size)
-    old=0
-    chunktotal=0
+  #  data=data[0:len(data)-len(data)%window_size].reshape(
+  #          int(len(data)/window_size),window_size)
     oldchunk=0
+    mean_stat=old_stat=np.zeros(len(statistics))
     #loop going through the chunks in the data in a random order
-    for i,h in enumerate(np.random.choice(range(2,len(data)),len(data)-2,replace=False)):
-        #calculates the running mean
-        new=(old*i+statistics(data,h))/(i+1)
-        old=new
-        chunktotal+=h
-        if(i%threshold_win==0 and i>threshold_win):
-            compare=abs(oldchunk-new)
+    for num_samples,sample_idx in enumerate(np.random.choice(
+            range(2,int(len(data)/window_size)),int(len(data)/window_size-2),replace=False)):
+        #loads the data from the random sample using generator
+        if(type(data)==EEG):
+            sample=generator(data,sample_idx,window_size).load()
+        else:
+            sample=generator(data,sample_idx,window_size)
+        #calculates the running mean for each statistic
+        for idx,stat in enumerate(statistics):
+            mean_stat[idx]=(old_stat[idx]*num_samples+stat(sample))/(num_samples+1)
+            old_stat[idx]=mean_stat[idx]
+        if(num_samples%threshold_win==0 and num_samples>threshold_win):
+            compare=abs(oldchunk-mean_stat[0])
             #compares the new and old mean to the threshold value
-            if (compare<threshold_mean):
+            if (compare<percent_tolerance/100*mean_stat[0]):
                 #because I am still testing it I am having it print out these values
-                print("samples_taken:",i,"difference",compare)
+                print("samples_taken",num_samples,"difference",compare)
                 break
-            oldchunk=new
-    return new
+            oldchunk=mean_stat[0]
+    return mean_stat
+
+def stabilize(data,window_size,stat,samples_taken):
+    """
+    Works with array or eeg object with one channel, makes a graph of the 
+    running mean for a statistic of randomly selected windows, used in order 
+    to see how long it takes for one particular statistic to stabilize
+    
+    args:
+        data(array or eeg object):     data used for ananlysis
+        window_size(int):              how large the chunks will be to
+                                       calculate a statistic from
+        stat(funtion):                 statistic used for analysis
+        samples_taken:                 number of random sample to be taken 
+                                        before stop
+    
+    Returns: graph x-axis is number of samples, x-axis is running mean 
+    of the statistic
+    """
+    old_stat=0
+    run_means=[]
+    for num_samples,sample_idx in enumerate(np.random.choice(
+                range(2,int(len(data)/window_size)),int(len(data)/window_size-2),replace=False)):
+        if(type(data)==EEG):
+            sample=generator(data,sample_idx,window_size).load()
+        else:
+            sample=generator(data,sample_idx,window_size)
+        mean_stat=(old_stat*num_samples+stat(sample))/(num_samples+1)
+        old_stat=mean_stat
+        run_means.append(mean_stat)
+        if num_samples>samples_taken:
+            break
+    plt.plot(run_means)
 
 
 
@@ -125,9 +152,5 @@ if __name__ == '__main__':
     samples = np.random.random((100,4))
     print(rms(samples))
     data=np.random.random(900000)*500
-    stat_estimator(data,rms,100,.5,20)
+    stat_estimator(data,[rms,coast],100,.5,20)  
     
-
-#the reason I added the place is so that the the statistics would also work
-#with the amplitude correlation needing three segments of data
-
